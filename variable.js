929ef782206f01a45e5a0eac5ddb65ad7649b2e1
jose-luis-hernandez-claros <joluhernandezc@correo.udistrital.edu.co>
2022-06-19 21:18:06 -0500

    comentarios

--- variable.js
+++ variable.js
@@ -1,6 +1,11 @@
+
+// var sirve para navegadores antiguos
 /*var x = 5; 
 var y=6;
-var z = x + y;*/
+var z = x + y;*/ 
+
+// const variables contantes.
+// Let variables no fijas.
 const x = 5;
 const y = 6; 
 let z = x*y;
