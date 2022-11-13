4bead1d36098c1ec1ce9701ce9b7dea67641907d
joseluishernandezclaros <joseluishc1966@gmail.com>
2022-11-12 19:39:04 -0500

    platzi

---
+++ platzi/servidor.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+function sitioweb(req, res){
+    res.writeHead(200, {"Content-Type": "text/html;"});
+    /* res.end("Mi primer servidor"); */
+    res.end("Mi <strong>segundo</string> servidor");
+}
+
+let servidor = http.createServer(sitioweb);
+
+servidor.listen(7070, "192.168.1.17");
+
+console.log("Servidor corriendo en el puerto 7070");
+
