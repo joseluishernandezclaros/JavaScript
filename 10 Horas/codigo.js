3000f7ebe5df59429686c3cb754e5a7138498047
joseluishernandezclaros <joseluishc1966@gmail.com>
2022-11-13 22:32:44 -0500

    monton de cambios

---
+++ 10 Horas/codigo.js
@@ -0,0 +1,92 @@
+/* String = "cadena de texto";
+Number = 19 //numero;
+booleano = true //false //true;
+ */
+
+/* var numero = 15;
+const nombre = "dato";
+nombre = "pedro"; //a las constantes no se les puede cambiar el valor
+ */
+
+/* let numero = 21;
+numero = 293;
+alert(numero); */
+
+/* const numero = 2;
+alert(numero); */
+
+/* let n1,n2,n3;
+n1=2;
+n2=23;
+n3=312;
+alert(n1);
+alert(n2);
+alert(n3); */
+
+/* let numero = null;
+alert(numero); */
+
+/* let numero = 5;
+let numero2 = "pedro";
+alert(numero*numero2); //NAN no es un numero
+ */
+
+/* let nombre = prompt("Escribe tu nombre");
+alert("hola "+nombre); */
+
+/* let numero = 10;
+numero += 20; //numero = numero + 20;
+document.write(numero); */
+
+/* let numero = 47;
+numero %= 6; //numero = numero % 20;
+document.write(numero); */
+
+/* let numero = 10;
+numero **= 2; //numero = numero elevado 2;
+document.write(numero); */
+
+/* n1 = 10;
+n2 = 5;
+resultado = n1 + n2;
+document.write(resultado); */
+
+/* n1 = 10;
+n2 = 5;
+n1--;
+resultado = n1;
+document.write(resultado); */
+
+/* n1 = 10;
+n2 = 5;
+resultado = n1 / n2;
+document.write(resultado); */
+
+/* n1 = 10;
+n2 = 5;
+resultado = n1 % n2; //5*1=5 , 5*2=10, 0
+document.write(resultado); */
+
+/* saludo = "Hola";
+pregunta = " ¿Como estas?";
+frase = saludo+pregunta;
+document.write(frase); */
+
+/* n1 = 5;
+n2 = 5;
+frase = "" + n1 + n2; //eran numeros pero se volvieron string
+document.write(frase); */
+
+/* n1 = "5";
+n2 = 5;
+frase = n1.concat(n2); //uso funcion concat
+document.write(frase); */
+
+/* frase1 = "jose";
+frasefinal = `soy ${frase1} y estoy caminando`; //backtig con alt 96
+document.write(frasefinal); */
+
+
+
+
+
