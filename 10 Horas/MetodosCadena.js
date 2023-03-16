ed62a4c2e5310ce896a6ecab357ae1e3a4645c30
joseluishernandezclaros <joseluishc1966@gmail.com>
2023-03-15 21:59:22 -0500

    ahi vamos

---
+++ 10 Horas/MetodosCadena.js
@@ -0,0 +1,19 @@
+
+let cadena = "cadena de prueba prueba prueba";
+let cadena2 = "cadena";
+
+// resultado = cadena.concat(cadena2); //concatenar o unir cadenas
+// resultado = cadena.startsWith(cadena2);  //verificar si una cadena comienza con la letra de otra cadena
+// resultado = cadena.endsWith(cadena2); //si las cadenas terminan con la misma letra
+// resultado = cadena.includes(cadena2); //si una cena contiene o incluye a otra cadena 
+// resultado = cadena.indexOf("de"); //posicion numerica de una cadena en especifico
+// resultado = cadena.lastIndexOf("prueba"); //ultima posicion en que se encuentra la palabra buscada
+
+
+
+
+
+
+
+
+document.write(resultado);
