47ad3c2a243f4b1b83c61dcc9fd373f51f0465fd
joseluishernandezclaros <joseluishc1966@gmail.com>
2023-03-30 23:32:36 -0500

    lol

--- 10 Horas/DOM/codigo2.js
+++ 10 Horas/DOM/codigo2.js
@@ -1,1 +1,12 @@
 
+const titulo = document.querySelector(".titulo");
+
+titulo.setAttribute("contentEditable","true"); //editar todo del atributo
+titulo.setAttribute("dir","ltr"); //cambiar como se muestra para leer el texto
+
+titulo.removeAttribute("hidden"); //hiden evita mostrar cosas asi que toca eliminarlo
+
+titulo.setAttribute("tabindex","true"); //ver si se puede seleccionar o no
+
+titulo.setAttribute("title","vea pues"); //titulo que se muestra al poner el mouse encima
+
---
+++ 10 Horas/DOM/codigo3.js
@@ -0,0 +1,17 @@
+
+const input = document.querySelector(".inputnormal"); //seleccionar clases
+
+// document.write(input.className); //nombre de la clase
+// document.write(input.value); //valor
+// document.write(input.type = "range"); //cambiar el type
+// document.write(input.accept = "image/png"); //permite configurar que tipo de imagen se pueden seleccionar
+// input.minLength = 4;    //cantidad minima de caracteres necesarios para enviar
+// input.setAttribute("minLength","4") //poner atributo de 4 caracteres    
+// input.placeholder = "a que pex"; //modifica directamente placeholder
+// input.required = " "; //configurar como el input como requerido es decir necesario si o si
+
+const titulo = document.querySelector(".titulo"); 
+// titulo.style.color = "red"; //cambiar color usando style
+// titulo.style.backgroundColor = "red"; //cambiar color fondo usando style
+titulo.style.padding = "30px"; //cambiar padding usando style
+
