0fb3a958d62632cc544cc1bb4e49407df582a99a
joseluishernandezclaros <joseluishc1966@gmail.com>
2023-03-31 14:20:22 -0500

    comentarios de explicacion propia

--- 10 Horas/coflaproblemasfinales/codigo.js
+++ 10 Horas/coflaproblemasfinales/codigo.js
@@ -1,10 +1,11 @@
 
 const contenedorTodo = document.querySelector(".flex-container");
-let contador = 0;
 const boton = document.querySelector(".send-button");
 let valorAntiguo = boton.value;
-boton.value = valorAntiguo.toUpperCase();
+boton.value = valorAntiguo.toUpperCase(); //cambiar el nombre de la clase antiguo por el mismo nombre pero en mayuscula
 
+
+let contador = 0;
 function crearLlave(nombre,modelo,precio){
     contador++;
     img = `<img src="llave.png" class="llave-img" <br>`;
@@ -20,20 +21,20 @@ function crearLlave(nombre,modelo,precio){
 
 // document.write(llave[0],llave[1],llave[2]);
 
-let documentFragment = document.createDocumentFragment();
+let documentFragment = document.createDocumentFragment(); //crea fragmento vacio que sirve para agregar varios elementos usando pocos recursos
 
 for (i = 1; i <= 20; i++) {
     let modeloRandom = Math.round(Math.random()*10000); //numero random 
     let precioRandom = Math.round(Math.random()*10+30); //numero random entre 30 y 40
-    let llave = crearLlave(`llave ${i}`,`modelo ${modeloRandom}`,precioRandom);
+    let llave = crearLlave(`llave ${i}`,`modelo ${modeloRandom}`,precioRandom); //llave 1 mande el nombre de la llave con diferente valor entre 1 y 20 manda el modelo y el preciorandom a la funcion crear llave
     let div = document.createElement("div");
     div.addEventListener("click", ()=>{
-        document.querySelector(".keydata").value = modeloRandom;
+        document.querySelector(".keydata").value = modeloRandom;   //se crea un div que cuando se pulsa un elemento se selleciona y cambia el valor del hidden por el valor del modeloRandom del elemento seleccionado
     });
-    div.tabIndex = i;
-    div.classList.add(`item-${i}`,`flexitem`); //añadir clases
-    div.innerHTML = llave[0]+llave[1]+llave[2]+llave[3];
-    documentFragment.appendChild(div);
+    div.tabIndex = i; //orden en que se selleciona usando el tab del teclado, en este caso es en orden de 1 a 20 
+    div.classList.add(`item-${i}`,`flexitem`); //añadir clase item con posicion i y clase flexitem
+    div.innerHTML = llave[0]+llave[1]+llave[2]+llave[3];  //añadir al div el valor de la llave en la posicion de imagen, nombre, modelo y precio
+    documentFragment.appendChild(div); //añade el div con todo lo agregado anteriormente al documentfragmente para que todo carge rapidamente y con mejor rendimiento
 }
 
-contenedorTodo.appendChild(documentFragment);
+contenedorTodo.appendChild(documentFragment); //agregar lo del documentfragment al contenedor creado en el index para ahora si mostrarlo en la pagina
